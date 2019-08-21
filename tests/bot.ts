3f60422117892e34530c3ff100b77de232d3c86e
sparklfox <actuallyori@gmail.com>
2019-08-21 21:07:09 +0100

    remove tests

--- tests/bot.ts
+++
@@ -1,8 +0,0 @@
-import * as floofi from "../src";
-import { test } from "./plugin";
-
-const bot = new floofi.Client({
-	debug: "verbose",
-}).pluginManager.add(test);
-
-bot.login("NDI5NjMxNjI1ODQwNjg5MTUy.XURtjw.FoN1Br9f89Unt7VenSWwuija7ic");
--- tests/plugin.ts
+++
@@ -1,5 +0,0 @@
-import { createPlugin } from "../src";
-
-export const test = createPlugin("test", (c) => {
-	c.logger.info("test plugin loaded.");
-});
